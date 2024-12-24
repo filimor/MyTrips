93b7fd1d9122b443ae06a5708f7f65a4ff325246
Filipe Moreira <filimor@posteo.com.br>
2024-12-24 20:07:15 -0300

    refactor: get all clients unit test

---
+++ src/MyTrips.Application/Services/ClientsService.cs
@@ -0,0 +1,11 @@
+﻿using MyTrips.Domain.Interfaces;
+
+namespace MyTrips.UnitTest;
+
+public class ClientsService(IClientRepository clientRepository)
+{
+    public async Task<object> GetClientsAsync()
+    {
+        return await clientRepository.GetAsync();
+    }
+}
---
+++ src/MyTrips.Domain/Entities/Client.cs
@@ -0,0 +1,8 @@
+﻿namespace MyTrips.Domain.Entities;
+
+public class Client
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+}
