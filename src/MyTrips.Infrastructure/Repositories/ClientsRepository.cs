b189ede6a152fef7ddf12772455ac56ee29ac4c3
Filimor <filimor@posteo.com.br>
2024-12-25 10:17:37 -0300

    tests: get all clients throws exception

--- src/MyTrips.Infrastructure/Repositories/ClientsRepository.cs
+++ src/MyTrips.Infrastructure/Repositories/ClientsRepository.cs
@@ -10,7 +10,7 @@ public class ClientsRepository : IClientsRepository
     public async Task<IEnumerable<Client>> GetAsync()
     {
         await using SqlConnection connection = new(
-            "Data Source=mytrips.database.windows.net;Initial Catalog=MyTripsDb;User ID=dbadmin;Password=@2eJgt57MhF6Zd$!5#4#;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            "Server=tcp:mytrips.database.windows.net,1433;Initial Catalog=MyTripsDb;Persist Security Info=False;User ID=dbadmin;Password=@2eJgt57MhF6Zd$!5#4#;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
         return connection.QueryAll<Client>();
     }
 }
