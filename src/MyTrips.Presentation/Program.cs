2b1fe27ea9a03a96367106045cd236ca03a04e7f
Filimor <filimor@posteo.com.br>
2024-12-25 00:51:11 -0300

    tests: get all clients integration test

--- src/MyTrips.Presentation/Program.cs
+++ src/MyTrips.Presentation/Program.cs
@@ -1,8 +1,21 @@
+using MyTrips.Application.Interfaces;
+using MyTrips.Application.Services;
+using MyTrips.Domain.Entities;
+using MyTrips.Domain.Interfaces;
+using MyTrips.Infrastructure.Repositories;
+using RepoDb;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IClientsRepository, ClientsRepository>();
+builder.Services.AddScoped<IClientsService, ClientsService>();
+
+GlobalConfiguration.Setup().UseSqlServer();
+FluentMapper.Entity<Client>().Table("Clients");
 
 var app = builder.Build();
 
