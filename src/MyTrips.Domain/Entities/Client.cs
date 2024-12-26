7137b27da2cf99370f9017f1a3c70cd3adde47fe
Filimor <filimor@posteo.com.br>
2024-12-26 15:46:12 -0300

    refactor: general code review

--- src/MyTrips.Domain/Entities/Client.cs
+++ src/MyTrips.Domain/Entities/Client.cs
@@ -3,6 +3,6 @@
 public class Client
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
+    public required string Name { get; set; }
+    public required string Email { get; set; }
 }
