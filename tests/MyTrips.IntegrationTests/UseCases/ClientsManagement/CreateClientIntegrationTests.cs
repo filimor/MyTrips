1d87cb46a941d31aedacfdf9dcc05d962a1b7f52
Filimor <filimor@posteo.com.br>
2024-12-27 14:33:20 -0300

    tests: create client with valid data integration test

---
+++ tests/MyTrips.IntegrationTests/UseCases/ClientsManagement/CreateClientIntegrationTests.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+using FluentAssertions;
+using MyTrips.Application.Dtos;
+using MyTrips.IntegrationTests.Extensions;
+using MyTrips.IntegrationTests.Fixtures;
+using Newtonsoft.Json;
+
+namespace MyTrips.IntegrationTests.UseCases.ClientsManagement;
+
+[Collection("ClientsManagement")]
+public class CreateClientIntegrationTests(ClientsManagementFixture fixture)
+{
+    [Fact]
+    [Trait("Category", "Integration")]
+    public async Task GivenValidClientDto_WhenRequestedPostClient_ThenItShouldReturnCreatedWithHeadersAndContent()
+    {
+        // Arrange
+        var json = JsonConvert.SerializeObject(fixture.RequestClientDtoStub);
+        StringContent data = new(json, Encoding.UTF8, "application/json");
+        var request = new HttpRequestMessage(HttpMethod.Post, fixture.Endpoint)
+        {
+            Content = data
+        };
+
+        // Act
+        var response = await fixture.DefaultHttpClient.SendAsync(request);
+
+        // Assert
+        var returnedClient = await response.DeserializedContentAsync<ResponseClientDto>();
+        response.EnsureSuccessStatusCode();
+        response.Should().HaveJsonContentType();
+        returnedClient.Should().BeEquivalentTo(fixture.ResponseClientDtoStub, options => options.Excluding(c => c.Id));
+        returnedClient!.Id.Should().BeGreaterThan(0);
+    }
+
+    //[Theory]
+    //[Trait("Category", "Integration")]
+    //[ClassData(typeof(InvalidStringClassData))]
+    //public async Task
+    //    GivenClientDtoWithInvalidName_WhenRequestedPostClient_ThenItShouldReturnBadRequestWithHeadersAndContent(
+    //        string name)
+    //{
+    //}
+
+    //[Theory]
+    //[Trait("Category", "Integration")]
+    //[ClassData(typeof(InvalidEmailClassData))]
+    //public async Task
+    //    GivenClientDtoWithInvalidEmail_WhenRequestedPostClient_ThenItShouldReturnBadRequestWithHeadersAndContent(
+    //        string email)
+    //{
+    //}
+
+    //[Fact]
+    //[Trait("Category", "Integration")]
+    //public async Task
+    //    GivenClientDtoWithExistingEmail_WhenRequestedPostClient_ThenItShouldReturnConflictWithHeadersAndContent()
+    //{
+    //}
+}
