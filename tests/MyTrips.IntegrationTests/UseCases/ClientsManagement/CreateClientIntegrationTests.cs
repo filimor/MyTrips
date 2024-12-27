147152f0fe9ab4bfa0daae1fcbe61f3b148d3a0f
Filimor <filimor@posteo.com.br>
2024-12-27 14:42:07 -0300

    tests: add client with invalid name integration test

--- tests/MyTrips.IntegrationTests/UseCases/ClientsManagement/CreateClientIntegrationTests.cs
+++ tests/MyTrips.IntegrationTests/UseCases/ClientsManagement/CreateClientIntegrationTests.cs
@@ -1,8 +1,12 @@
-﻿using System.Text;
+﻿using System.Net;
+using System.Text;
 using FluentAssertions;
 using MyTrips.Application.Dtos;
+using MyTrips.Domain.Entities;
 using MyTrips.IntegrationTests.Extensions;
 using MyTrips.IntegrationTests.Fixtures;
+using MyTrips.Presentation.Errors;
+using MyTrips.UnitTest.ClassData;
 using Newtonsoft.Json;
 
 namespace MyTrips.IntegrationTests.UseCases.ClientsManagement;
@@ -33,14 +37,31 @@ public class CreateClientIntegrationTests(ClientsManagementFixture fixture)
         returnedClient!.Id.Should().BeGreaterThan(0);
     }
 
-    //[Theory]
-    //[Trait("Category", "Integration")]
-    //[ClassData(typeof(InvalidStringClassData))]
-    //public async Task
-    //    GivenClientDtoWithInvalidName_WhenRequestedPostClient_ThenItShouldReturnBadRequestWithHeadersAndContent(
-    //        string name)
-    //{
-    //}
+    [Theory]
+    [Trait("Category", "Integration")]
+    [ClassData(typeof(InvalidStringClassData))]
+    public async Task
+        GivenClientDtoWithInvalidName_WhenRequestedPostClient_ThenItShouldReturnBadRequestWithHeadersAndContent(
+            string name)
+    {
+        // Arrange
+        fixture.RequestClientDtoStub.Name = name;
+        var json = JsonConvert.SerializeObject(fixture.RequestClientDtoStub);
+        StringContent data = new(json, Encoding.UTF8, "application/json");
+        var request = new HttpRequestMessage(HttpMethod.Post, fixture.Endpoint)
+        {
+            Content = data
+        };
+
+        // Act
+        var response = await fixture.DefaultHttpClient.SendAsync(request);
+
+        // Assert
+        var errorDetails = await response.DeserializedContentAsync<ErrorDetails>();
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        response.Should().HaveProblemContentType();
+        errorDetails!.Errors.Should().ContainMatch($"*{nameof(Client.Name)}*");
+    }
 
     //[Theory]
     //[Trait("Category", "Integration")]
