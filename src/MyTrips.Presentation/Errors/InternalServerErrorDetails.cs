32e37420e1510697d01df7149257d6bdd5ad8b27
Filimor <filimor@posteo.com.br>
2024-12-27 14:58:47 -0300

    refactor: add better details messages for the ErrorDetails objects

--- src/MyTrips.Presentation/Errors/InternalServerErrorDetails.cs
+++ src/MyTrips.Presentation/Errors/InternalServerErrorDetails.cs
@@ -3,7 +3,8 @@
 public class InternalServerErrorDetails : ErrorDetails
 {
     public InternalServerErrorDetails(HttpContext context,
-        string detail = "An unexpected error occurred. Please try again later.")
+        string detail =
+            "The server encountered an unexpected condition that prevented it from fulfilling the request. This is typically a temporary issue, and our team is working to resolve it as quickly as possible. Please try again later. If the problem persists, contact support for further assistance.")
     {
         Status = StatusCodes.Status500InternalServerError;
         Title = "Internal Server Error";
