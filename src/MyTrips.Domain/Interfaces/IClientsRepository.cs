1680a96295ae4d99008739b28319437fc5f06d3f
Filimor <filimor@posteo.com.br>
2024-12-26 23:13:20 -0300

    tests: create client with already existing email unit test
    Add FindAsync method to the repository

--- src/MyTrips.Domain/Interfaces/IClientsRepository.cs
+++ src/MyTrips.Domain/Interfaces/IClientsRepository.cs
@@ -1,11 +1,12 @@
-﻿using MyTrips.Domain.Entities;
+﻿using System.Linq.Expressions;
+using MyTrips.Domain.Entities;
 
 namespace MyTrips.Domain.Interfaces;
 
 public interface IClientsRepository
 {
-    string ConnectionString { get; }
     Task<IEnumerable<Client>> GetAsync();
     Task<Client?> GetAsync(int id);
     Task<int> AddAsync(Client client);
+    Task<IEnumerable<Client>> FindAsync(Expression<Func<Client, bool>> predicate);
 }
