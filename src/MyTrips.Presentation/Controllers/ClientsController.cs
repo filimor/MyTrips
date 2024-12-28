023e38910c86c2ca876ecbba315a2db19ee18445
Filimor <filimor@posteo.com.br>
2024-12-28 10:27:06 -0300

    docs: add the API documentation

--- src/MyTrips.Presentation/Controllers/ClientsController.cs
+++ src/MyTrips.Presentation/Controllers/ClientsController.cs
@@ -14,9 +14,21 @@ namespace MyTrips.Presentation.Controllers;
 [ApiController]
 public class ClientsController(IClientsService clientsService, IValidator<Client> validator) : ControllerBase
 {
+    /// <summary>Get all clients</summary>
+    /// <remarks>
+    /// <b>Sample request:</b>
+    /// <br />
+    /// <pre>
+    /// GET /api/clients/
+    /// </pre>
+    /// </remarks>
+    /// <returns>A list of all clients</returns>
+    /// <response code="200">Returns a list of all clients</response>
+    /// <response code="500">If an error occurs while processing the request</response>
     [HttpGet]
     [ProducesResponseType<IEnumerable<ResponseClientDto>>(StatusCodes.Status200OK)]
     [ProducesResponseType<ErrorDetails>(StatusCodes.Status500InternalServerError)]
+    [Produces("application/json")]
     public async Task<ActionResult> Get()
     {
         var result = await clientsService.GetClientsAsync();
@@ -24,11 +36,26 @@ public class ClientsController(IClientsService clientsService, IValidator<Client
         return Ok(result.Value);
     }
 
+    /// <summary>Get a client by id</summary>
+    /// <remarks>
+    /// <b>Sample request:</b>
+    /// <br />
+    /// <pre>
+    /// GET /api/clients/1
+    /// </pre>
+    /// </remarks>
+    /// <param name="id"> The id of the client to get</param>
+    /// <returns>A client with the specified id</returns>
+    /// <response code="200">Returns the client with the specified id</response>
+    /// <response code="400">If the id is less than 1</response>
+    /// <response code="404">If the client with the specified id is not found</response>
+    /// <response code="500">If an error occurs while processing the request</response>
     [HttpGet("{id:int}")]
     [ProducesResponseType<ResponseClientDto>(StatusCodes.Status200OK)]
     [ProducesResponseType<ErrorDetails>(StatusCodes.Status400BadRequest)]
     [ProducesResponseType<ErrorDetails>(StatusCodes.Status404NotFound)]
     [ProducesResponseType<ErrorDetails>(StatusCodes.Status500InternalServerError)]
+    [Produces("application/json")]
     public async Task<ActionResult> Get([FromRoute] int id)
     {
         var validationResult = ValidateInputId(id);
@@ -55,11 +82,33 @@ public class ClientsController(IClientsService clientsService, IValidator<Client
         return Ok(requestResult.Value);
     }
 
+    /// <summary>Create a new client</summary>
+    /// <remarks>
+    /// <b>Sample request:</b>
+    /// <br />
+    /// <pre>
+    /// <b>NOTE:</b> The e-mail must be unique.
+    /// <br />
+    /// POST /api/clients
+    /// {
+    ///   "name": "John Doe",
+    ///   "email": "john.doe@example.com"
+    /// }
+    /// </pre>
+    /// </remarks>
+    /// <param name="createClientDto">The data to create a new client</param>
+    /// <returns>The newly created client</returns>
+    /// <response code="201">Returns the newly created client</response>
+    /// <response code="400">If any of the attributes are invalid</response>
+    /// <response code="409">If a client with the same email already exist</response>
+    /// <response code="500">If an error occurs while processing the request</response>
     [HttpPost]
     [ProducesResponseType<ResponseClientDto>(StatusCodes.Status201Created)]
     [ProducesResponseType<ErrorDetails>(StatusCodes.Status400BadRequest)]
     [ProducesResponseType<ErrorDetails>(StatusCodes.Status409Conflict)]
     [ProducesResponseType<ErrorDetails>(StatusCodes.Status500InternalServerError)]
+    [Produces("application/json")]
+    [Consumes("application/json")]
     public async Task<ActionResult> Post([FromBody] CreateClientDto createClientDto)
     {
         var client = new Client(createClientDto.Name, createClientDto.Email);
@@ -90,12 +139,34 @@ public class ClientsController(IClientsService clientsService, IValidator<Client
         return CreatedAtAction(nameof(Get), new { id = requestResult.Value.Id }, requestResult.Value);
     }
 
+    /// <summary>Update a client</summary>
+    /// <remarks>
+    /// <b>Sample request:</b>
+    /// <br />
+    /// <pre>
+    /// PUT /api/clients
+    /// {
+    ///   "id": 1,
+    ///   "name": "Jane Doe",
+    ///   "email": "jane.doe@example.com"
+    /// }
+    /// </pre>
+    /// </remarks>
+    /// <param name="updateClientDto">The data to update a client</param>
+    /// <returns>The updated client</returns>
+    /// <response code="200">Returns the updated client</response>
+    /// <response code="400">If the id is less than 1 or any of the others attributes are invalid</response>
+    /// <response code="404">If the client with the specified id is not found</response>
+    /// <response code="409">If a client with the same id already exist</response>
+    /// <response code="500">If an error occurs while processing the request</response>
     [HttpPut]
     [ProducesResponseType<ResponseClientDto>(StatusCodes.Status200OK)]
     [ProducesResponseType<ErrorDetails>(StatusCodes.Status400BadRequest)]
     [ProducesResponseType<ErrorDetails>(StatusCodes.Status404NotFound)]
     [ProducesResponseType<ErrorDetails>(StatusCodes.Status409Conflict)]
     [ProducesResponseType<ErrorDetails>(StatusCodes.Status500InternalServerError)]
+    [Produces("application/json")]
+    [Consumes("application/json")]
     public async Task<ActionResult> Put([FromBody] UpdateClientDto updateClientDto)
     {
         var idValidationResult = ValidateInputId(updateClientDto.Id);
@@ -140,11 +211,26 @@ public class ClientsController(IClientsService clientsService, IValidator<Client
         return Ok(requestResult.Value);
     }
 
+    /// <summary>Delete a client</summary>
+    /// <remarks>
+    /// <b>Sample request:</b>
+    /// <br />
+    /// <pre>
+    /// DELETE /api/clients/1
+    /// </pre>
+    /// </remarks>
+    /// <param name="id">The id of the client to delete</param>
+    /// <returns>No content</returns>
+    /// <response code="204">If the client was successful deleted</response>
+    /// <response code="400">If the id is less than 1</response>
+    /// <response code="404">If the client with the specified id is not found</response>
+    /// <response code="500">If an error occurs while processing the request</response>
     [HttpDelete("{id:int}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType<ErrorDetails>(StatusCodes.Status400BadRequest)]
     [ProducesResponseType<ErrorDetails>(StatusCodes.Status404NotFound)]
     [ProducesResponseType<ErrorDetails>(StatusCodes.Status500InternalServerError)]
+    [Produces("application/json")]
     public async Task<ActionResult> Delete([FromRoute] int id)
     {
         var validationResult = ValidateInputId(id);
---
+++ src/MyTrips.Presentation/Filters/SwaggerSchemaFilter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.OpenApi.Models;
+using MyTrips.Application.Dtos;
+using MyTrips.Presentation.Errors;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace MyTrips.Presentation.Filters;
+
+public class SwaggerSchemaFilter : ISchemaFilter
+{
+    public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+    {
+        if (context.Type == typeof(CreateClientDto)) schema.Title = "Create Client Request";
+        else if (context.Type == typeof(ErrorDetails)) schema.Title = "Error Response";
+        else if (context.Type == typeof(ResponseClientDto)) schema.Title = "Client Response";
+        else if (context.Type == typeof(UpdateClientDto)) schema.Title = "Update Client Request";
+    }
+}
