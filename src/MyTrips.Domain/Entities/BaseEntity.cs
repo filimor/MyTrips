e4a8b53a48dd91aff1142331c23e838290608f8f
Filimor <filimor@posteo.com.br>
2024-12-30 03:59:19 -0300

    refactor: add base entity equality operators

--- src/MyTrips.Domain/Entities/BaseEntity.cs
+++ src/MyTrips.Domain/Entities/BaseEntity.cs
@@ -1,7 +1,17 @@
-﻿namespace MyTrips.Domain.Entities;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MyTrips.Domain.Entities;
 
 public abstract class BaseEntity
 {
+    private static readonly JsonSerializerOptions JsonOptions = new()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = true,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+    };
+
     protected BaseEntity()
     {
     }
@@ -12,4 +22,40 @@ public abstract class BaseEntity
     }
 
     public int Id { get; set; }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null || GetType() != obj.GetType()) return false;
+
+        var other = (BaseEntity)obj;
+
+        return Id == other.Id;
+    }
+
+    public virtual bool Equals(BaseEntity? other)
+    {
+        return other is not null && Id == other.Id;
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+
+    public override string ToString()
+    {
+        return JsonSerializer.Serialize(this, JsonOptions);
+    }
+
+    public static bool operator ==(BaseEntity? a, BaseEntity? b)
+    {
+        if (a is null && b is null) return true;
+        if (a is null || b is null) return false;
+        return a.Equals(b);
+    }
+
+    public static bool operator !=(BaseEntity? a, BaseEntity? b)
+    {
+        return !(a == b);
+    }
 }
